247644ea29e68c4929f81424d7227870d8a1372e
Jun Nie <jun.nie@linaro.org>
2015-03-14 19:49:36 +0800

    ARM: zx: add basic support of ZTE ZX296702
    Add basic code for ZTE ZX296702 platform.
    
    Signed-off-by: Jun Nie <jun.nie@linaro.org>

---
+++ arch/arm/mach-zx/zx296702.c
@@ -0,0 +1,20 @@
+/*
+ * Copyright 2014 Linaro Ltd.
+ * Copyright (C) 2014 ZTE Corporation.
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License version 2 as
+ * published by the Free Software Foundation.
+ */
+
+#include <asm/mach/arch.h>
+#include <asm/mach/map.h>
+
+static const char *zx296702_dt_compat[] __initconst = {
+	"zte,zx296702",
+	NULL,
+};
+
+DT_MACHINE_START(ZX, "ZTE ZX296702 (Device Tree)")
+	.dt_compat	= zx296702_dt_compat,
+MACHINE_END
