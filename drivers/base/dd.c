32f9b988ec3baf96f7ea09cf0751202dbb13f064
Sricharan R <sricharan@codeaurora.org>
2016-08-09 04:19:07 +0530

    drivers: platform: Configure dma operations at probe time
    Configuring DMA ops at probe time will allow deferring device probe when
    the IOMMU isn't available yet. The dma_configure for the device is now called
    from the generic device_attach callback just before the bus/driver probe
    is called. This way, configuring the dma ops for the device would be called
    at the same place for all bus_types, hence the deferred probing mechanism
    should work for all buses as well.
    
    pci_bus_add_devices    (platform/amba)(_device_create/driver_register)
           |                         |
    pci_bus_add_device     (device_add/driver_register)
           |                         |
    device_attach           device_initial_probe
           |                         |
    __device_attach_driver    __device_attach_driver
           |
    driver_probe_device
           |
    really_probe
           |
    dma_configure
    
     Similarly on the device/driver_unregister path __device_release_driver is
     called which inturn calls dma_deconfigure.
    
    Signed-off-by: Sricharan R <sricharan@codeaurora.org>

--- drivers/base/dd.c
+++ drivers/base/dd.c
@@ -19,6 +19,7 @@
 
 #include <linux/device.h>
 #include <linux/delay.h>
+#include <linux/dma-mapping.h>
 #include <linux/module.h>
 #include <linux/kthread.h>
 #include <linux/wait.h>
@@ -356,6 +357,10 @@ re_probe:
 	if (ret)
 		goto pinctrl_bind_failed;
 
+	ret = dma_configure(dev);
+	if (ret)
+		goto dma_failed;
+
 	if (driver_sysfs_add(dev)) {
 		printk(KERN_ERR "%s: driver_sysfs_add(%s) failed\n",
 			__func__, dev_name(dev));
@@ -417,6 +422,8 @@ re_probe:
 	goto done;
 
 probe_failed:
+	dma_deconfigure(dev);
+dma_failed:
 	if (dev->bus)
 		blocking_notifier_call_chain(&dev->bus->p->bus_notifier,
 					     BUS_NOTIFY_DRIVER_NOT_BOUND, dev);
@@ -825,6 +832,7 @@ static void __device_release_driver(struct device *dev, struct device *parent)
 		else if (drv->remove)
 			drv->remove(dev);
 
+		dma_deconfigure(dev);
 		device_links_driver_cleanup(dev);
 		devres_release_all(dev);
 		dev->driver = NULL;
